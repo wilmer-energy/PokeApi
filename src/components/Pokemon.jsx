8198e0fca3c53f56fdaff731bc017dff23e6a502
WILMER <wcaceres84@unab.edu.co>
2022-07-04 20:54:58 -0500

    title added

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import axios from "axios";
 import { useState, useEffect } from "react";
-
+/*
+YA ESTAN LOS POKEMONES EN UN ARRAY DE OBJETOS, HAY QUE IMPRIMIR VARIAS CARTAS EN REACT
+LA OTRA SEMANA NOS ENSEÑAN ESO */
 const Pokemon = () => {
     const [pokemon, setPokemon] = useState({});
     const [height, setHeight] = useState(0);
@@ -38,25 +40,25 @@ const Pokemon = () => {
             setHeight(res.data.height);
         });
     }
-        return (
-            <div className="App">
-                <div className="card">
-                    <h1>{pokemon.name}</h1>
-                    <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
-                    <div>
-                        <ul>
-                            <li>Weight:  <span>{pokemon.weight}</span></li>
-                            <li>Height: <span>{isDecimeters ? height + " decimeters" : height / 10 + " meters"}</span></li>
-                            <li>Type: <span>{pokemon.types?.[0].type.name}</span></li>
-                        </ul>
+    return (
+        <div className="App">
+            <div className="card">
+                <h1>{pokemon.name}</h1>
+                <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
+                <div>
+                    <ul>
+                        <li>Weight:  <span>{pokemon.weight}</span></li>
+                        <li>Height: <span>{isDecimeters ? height + " decimeters" : height / 10 + " meters"}</span></li>
+                        <li>Type: <span>{pokemon.types?.[0].type.name}</span></li>
+                    </ul>
 
-                    </div>
-                    <button onClick={convertHeight}>Convert to meters</button><br />
-                    <button onClick={changePokemon}>Change Pokemon</button>
                 </div>
+                <button onClick={convertHeight}>Convert to meters</button><br />
+                <button onClick={changePokemon}>Change Pokemon</button>
             </div>
-        );
-    
+        </div>
+    );
+
 
 };
 
